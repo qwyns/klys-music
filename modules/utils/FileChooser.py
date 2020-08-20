b8c8eecdef693c1f50f2805dafc6ed1a484e2692
qwyns <ashwinddalvi@gmail.com>
2020-08-20 19:28:08 +0530

    FileChooser committed

--- modules/utils/FileChooser.py
+++ modules/utils/FileChooser.py
@@ -1,4 +1,7 @@
-import os, math, sys
+import os
+import math
+import sys
+
 
 def FileChooser(i):
     PrintDirectories(os.path.realpath(i))
@@ -10,12 +13,14 @@ def FileChooser(i):
         if os.path.isdir(n):
             return FileChooser(n)
         else:
-            print (f"{choice} is not a valid directory")
+            print(f"{choice} is not a valid directory")
             return FileChooser(i)
 
+
 def PrintDirectories(i):
-    dirlist = ['. (Choose)', '.. (Parent)', *[f.name for f in os.scandir(i) if f.is_dir()]]
+    dirlist = ['. (Choose)', '.. (Parent)', *
+               [f.name for f in os.scandir(i) if f.is_dir()]]
     longest_name_len = max([len(_) for _ in dirlist])
     entries_per_row = math.ceil(140/(longest_name_len + 4))
-    print ('\n'.join([''.join((*map(lambda x: x.ljust(longest_name_len + 4), dirlist[_:_+entries_per_row]), )) for _ in range(0, len(dirlist), entries_per_row)]))
-
+    print('\n'.join([''.join((*map(lambda x: x.ljust(longest_name_len + 4), dirlist[_:_+entries_per_row]), ))
+                     for _ in range(0, len(dirlist), entries_per_row)]))
